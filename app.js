f15ade372dce4af94b3e56295c3c0f232bf70d6c
Carl Olson <colson10@gmail.com>
2018-01-12 14:21:08 -0800

    solved problem 3

--- app.js
+++ app.js
@@ -52,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var sumOfNums = a + b + c;
+  var productOfNums = a * b * c;
+  var sumMessage = a + ' and ' + b + ' and ' + c + ' sum to 16.';
+  var productMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is 140.';
+  return [sumOfNums, productOfNums, sumMessage, productMessage];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
